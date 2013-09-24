76be5e6cd8b78e41fc0080b3c5fde38fee4b1d87
Nathan Butler <iamnbutler@gmail.com>
2013-09-24 03:46:35 -0400

    Updated lb2 to current codepen status, continual css sorting

---
+++ js/scripts-ck.js
@@ -0,0 +1,2 @@
+// Load scripts after page loads
+$(document).ready(function(){$(".inactive").click(function(){return!1});$(".menu-toggle").click(function(){$(".body-frame").toggleClass("expanded")})});
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 // Load scripts after page loads
 $(document).ready(function(){
-
+  $('.inactive').click(function(){
+    return false;
+  });
+  
+  $('.menu-toggle').click(function(){
+    $('.body-frame').toggleClass('expanded');
+  });
 });
